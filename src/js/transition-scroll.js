d4d8d20071d0a54c95b383581e9af1f5a9662843
korsikov009 <korsikov009@gmail.com>
2019-02-10 02:51:28 +0300

    ALL did transition scroll

--- src/js/transition-scroll.js
+++ src/js/transition-scroll.js
@@ -1,29 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-   const menu = document.querySelector('.menu');
-   const button = document.querySelector('.navigation__button');
-
-   function showMenu() {
-       button.classList.add('navigation__button_active');
-       menu.classList.add('menu_visible');
-       document.addEventListener('click', hideMenu);
-   }
-
-   function hideMenu() {
-       button.classList.remove('navigation__button_active');
-       menu.classList.remove('menu_visible');
-       document.removeEventListener('click', hideMenu);
-   }
-
-   button.addEventListener('click', event => {
-       if (!menu.classList.contains('menu_visible')) {
-           showMenu();
-           event.stopPropagation();
-       } else {
-           hideMenu();
-       }
-   });
-});
-document.addEventListener('DOMContentLoaded', () => {
     const element = document.querySelector('.main-content__title');
 
     document.querySelector('.header__anchor').addEventListener('click', event => {
