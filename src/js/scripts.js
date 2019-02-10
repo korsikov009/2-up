cc7e4893fc2504215596954e20d9de997688a635
korsikov009 <korsikov009@gmail.com>
2019-02-10 15:12:48 +0300

    MODAL fixed body scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -30,17 +30,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const modalClose = document.querySelector('.modal__close');
     const body = document.querySelector('body');
     const scrollWidth = window.outerWidth - document.documentElement.clientWidth;
+    let scrollTop = 0;
 
     function modalShow() {
+        scrollTop = document.documentElement.scrollTop;
+        body.style.top = -scrollTop + 'px';
         body.classList.add('modal_open');
         body.style.paddingRight = scrollWidth + 'px';
         modalBackground.style.display = 'flex';
         modalBackground.classList.add('modal_show');
-
     }
 
     function modalHide() {
         body.classList.remove('modal_open');
+        window.scrollTo(0, scrollTop);
         body.style.paddingRight = '';
         modalBackground.style.display = 'none';
         modalBackground.classList.remove('modal_show');
